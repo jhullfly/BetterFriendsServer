6736a1e89f99006ffad4541c914afb56df4fc423
Jesse Hull <jessehull@gmail.com>
2014-10-17 14:55:22 -0700

    Full support for cors

--- config/express.js
+++ config/express.js
@@ -34,6 +34,15 @@ module.exports = function(db) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
+  app.options('*', function(req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.send();
+  });
+
+
 
 	// Passing the request url to environment locals
 	app.use(function(req, res, next) {

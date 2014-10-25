55785bf43268c88bde20c28e88c26685d79ff11c
Jesse Hull <jessehull@gmail.com>
2014-10-24 21:00:10 -0700

    fix links on install page

--- public/landing/application.client.js
+++ public/landing/application.client.js
@@ -41,7 +41,8 @@ angular.module('betterfriendsLanding').controller('AcceptedController', ['$scope
       var clickedAt = new Date();
       $timeout(function () {
         if (new Date() - clickedAt < 2000) {
-          $window.location.href = 'http://www.testflightapp.com/install/ba99bd94a202a6c4b4142ee99b83eac7-MTM3NDk0NTQ/'; //itms-apps://itunes.apple.com/us/app/facebook/id284882215?mt=8&uo=6';
+          $window.location.href = 'http://www.testflightapp.com/install/b0917ddaaf8336dda336598c7613a26e-MTM3NDk5Nzk/';
+          //itms-apps://itunes.apple.com/us/app/facebook/id284882215?mt=8&uo=6';
         }
       }, 500);
       var data = {
@@ -49,7 +50,8 @@ angular.module('betterfriendsLanding').controller('AcceptedController', ['$scope
         inviteCode:$stateParams.inviteCode
       };
       var dataString = encodeURIComponent('j:'+JSON.stringify(data));
-      $window.location.href = 'betterfriends://cookiedata?data='+dataString;
+      //TODO: off for now to force upgrade.
+      //$window.location.href = 'betterfriends://cookiedata?data='+dataString;
     };
     $http.get('/event/get/'+$stateParams.eid+'/'+$stateParams.inviteCode).then(function (res) {
       $scope.event = res.data.event;
